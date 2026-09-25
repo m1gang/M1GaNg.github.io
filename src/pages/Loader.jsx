6d3315f0e2b3c8748fd855ec76828d07aefa8e10
MiGaNg <miguelangelyv1@gmail.com>
2026-09-25 11:29:28 -0500

    chore: guardar cambio menor de Loader y nuevos SVG sin relacion con port-3

--- src/pages/Loader.jsx
+++ src/pages/Loader.jsx
@@ -32,7 +32,6 @@ export const Loader = () => {
   // centered container and scaled to a smaller size for a cleaner loader UI.
   return (
     <div
-
       style={{
         width: "100%",
         height: "100vh",
